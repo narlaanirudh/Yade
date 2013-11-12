392b738b4d7f7416689c467050d8e32e063ccff2
Anirudh Narla <narlaanirudh@gmail.com>
2013-11-12 10:24:28 +0000

    Initial Commit

---
+++ Ribbit/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  Ribbit
+//
+//  Created by Anirudh narla on 11/12/13.
+//  Copyright (c) 2013 Anirudh Narla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
